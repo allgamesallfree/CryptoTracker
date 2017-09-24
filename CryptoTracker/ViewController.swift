6ec3180ffece3d15620c5c6bceca6bf48b9a5278
Maxwell Stein <max34563@gmail.com>
2017-09-23 20:29:51 -0400

    Add initial UI for Ethereum price and IBOutlet

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Maxwell Stein on 9/23/17.
+//  Copyright © 2017 Maxwell Stein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var etherValueLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
