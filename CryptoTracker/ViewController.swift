2e5079326eebea6db9093a72fc7d3618f9cb6585
Maxwell Stein <max34563@gmail.com>
2017-10-06 10:38:50 -0400

    Update spacing to 4 lines for tab instead of 2

--- CryptoTracker/ViewController.swift
+++ CryptoTracker/ViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var etherValueLabel: UILabel!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    @IBOutlet weak var etherValueLabel: UILabel!
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 
 }
-
