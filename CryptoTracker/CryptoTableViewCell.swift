7dc297a3c951832d78af33e4626d40715e4146ce
allgamesallfree <max34563@gmail.com>
2017-12-14 14:29:07 -0500

    Update with tableview and image assets

---
+++ CryptoTracker/CryptoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoTracker
+//
+//  Created by Maxwell Stein on 12/14/17.
+//  Copyright © 2017 Maxwell Stein. All rights reserved.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var currencyImageView: UIImageView!
+    @IBOutlet weak var currencyName: UILabel!
+    @IBOutlet weak var currencyPrice: UILabel!
+
+}
---
+++ CryptoTracker/CryptoTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoTableViewController.swift
+//  CryptoTracker
+//
+//  Created by Maxwell Stein on 12/13/17.
+//  Copyright © 2017 Maxwell Stein. All rights reserved.
+//
+
+import UIKit
+
+final class CryptoTableViewController: UITableViewController {
+
+}
+
